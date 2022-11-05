0812e43242f83aeb776408a9a31e98159833d5a4
infintygalaxy <gauravrajan101@gmail.com>
2022-11-05 14:45:46 +0530

    Updates

---
+++ precache-manifest.eaef69ab47e568c6a74e818f8a00ba33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "163cfca29771f67edc5f",
+    "url": "/textutils/static/css/main.ce03bb58.chunk.css"
+  },
+  {
+    "revision": "163cfca29771f67edc5f",
+    "url": "/textutils/static/js/main.163cfca2.chunk.js"
+  },
+  {
+    "revision": "31088909e753e45fc079",
+    "url": "/textutils/static/js/1.31088909.chunk.js"
+  },
+  {
+    "revision": "da93abcda7bd6182c383",
+    "url": "/textutils/static/js/2.da93abcd.chunk.js"
+  },
+  {
+    "revision": "912e2fae093cb43f4f59",
+    "url": "/textutils/static/js/runtime~main.912e2fae.js"
+  },
+  {
+    "revision": "bba5406f45f103a1c2b8d3de22ab2c71",
+    "url": "/textutils/index.html"
+  }
+];
